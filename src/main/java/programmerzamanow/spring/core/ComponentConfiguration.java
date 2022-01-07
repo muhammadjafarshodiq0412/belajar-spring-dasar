b3cda6504a8d24040e14de6eddfb0506212b96e7
jafar1204 <muhammad.shodiq@pgi-data.com>
2022-01-07 12:53:48 +0700

    [FEAT] ADD Pertemuan 22 Componant Annotation

---
+++ src/main/java/programmerzamanow/spring/core/ComponentConfiguration.java
@@ -0,0 +1,11 @@
+package programmerzamanow.spring.core;
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@ComponentScan(basePackages = {
+        "programmerzamanow.spring.core.service"
+})
+public class ComponentConfiguration {
+}
---
+++ src/main/java/programmerzamanow/spring/core/service/ProductService.java
@@ -0,0 +1,7 @@
+package programmerzamanow.spring.core.service;
+
+import org.springframework.stereotype.Component;
+
+@Component
+public class ProductService {
+}
